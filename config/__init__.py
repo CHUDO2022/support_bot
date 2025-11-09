38931ea544d8c618cc262d56c0546832778311b8
CHUDO <ahmetovural.ru@gmail.com>
2025-11-09 15:27:30 +0300

    агрузкаи новой версии преокта 2)

--- config/__init__.py
+++ config/__init__.py
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
 """
 Модуль конфигурации приложения
 """
 from .settings import Settings, get_settings
 
 __all__ = ['Settings', 'get_settings']
-
-=======
-"""
-Модуль конфигурации приложения
-"""
-from .settings import Settings, get_settings
-
-__all__ = ['Settings', 'get_settings']
-
-
-
->>>>>>> 92da3f4 (окальный старт)
